5bd9aaf6a140286c39fe1ec0c721243510838a75
vsec2025 <vietsafe.ec@gmail.com>
2026-06-04 00:15:43 +0700

    fix: next.config.js - them bcryptjs external

--- webapp/next.config.js
+++ webapp/next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: ['bcryptjs'],
+  },
 }
 module.exports = nextConfig
